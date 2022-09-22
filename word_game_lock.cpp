4a4cfbd039bc623c035effa5ab43cb101bba3f99
LIN, Ziyuan <ziyuang@gmail.com>
2022-09-22 20:23:56 +0300

    type alias for the char array

--- word_game_lock.cpp
+++ word_game_lock.cpp
@@ -5,8 +5,11 @@
 #include "general_solver.h"
 
 class WordLockState : public BaseState<WordLockState, std::string> {
+private:
+  using Chars = std::array<std::string, 7>;
+
 public:
-  WordLockState(const std::array<std::string, 7> &characters) : c_(characters) {}
+  WordLockState(const Chars &characters) : c_(characters) {}
   size_t hash() const {
     // https://stackoverflow.com/a/72073933/688080
     return std::accumulate(c_.begin(), c_.end(), c_.size(), [](std::size_t h, const std::string &mbchar) {
@@ -26,20 +29,15 @@ public:
   }
   bool isFinal() const { return (*this) == answer_; }
   std::vector<State_Move> nextStates() {
-    return std::vector<State_Move>{{WordLockState(rotateMiddle()), "middle"},
-                                      {WordLockState(rotateRight()), "right"}};
+    return std::vector<State_Move>{{WordLockState(rotateMiddle()), "middle"}, {WordLockState(rotateRight()), "right"}};
   }
 
 private:
-  std::array<std::string, 7> c_;
+  Chars c_;
   static const WordLockState answer_;
 
-  std::array<std::string, 7> rotateMiddle() {
-    return std::array<std::string, 7>{c_[2], c_[1], c_[5], c_[0], c_[4], c_[3], c_[6]};
-  }
-  std::array<std::string, 7> rotateRight() {
-    return std::array<std::string, 7>{c_[0], c_[3], c_[2], c_[6], c_[1], c_[5], c_[4]};
-  }
+  Chars rotateMiddle() { return Chars{c_[2], c_[1], c_[5], c_[0], c_[4], c_[3], c_[6]}; }
+  Chars rotateRight() { return Chars{c_[0], c_[3], c_[2], c_[6], c_[1], c_[5], c_[4]}; }
 };
 
 const WordLockState WordLockState::answer_({"的", "蠢", "封", "连", "的", "可", "维"});
